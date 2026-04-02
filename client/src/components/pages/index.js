8cb71c2896ff23a0714a3c4cb7a8528f2f695fec
arpitsharma2010 <arpeet.sharma.1998@gmail.com>
2026-04-02 11:44:15 -0400

    feat(auth): add marvin self-registration flow

--- client/src/components/pages/index.js
+++ client/src/components/pages/index.js
@@ -4,3 +4,4 @@ export { default as DownloadsPage } from './DownloadsPage';
 export { default as PrivacyPage } from './PrivacyPage';
 export { default as ResearchPage } from './ResearchPage';
 export { default as BlogArchivePage } from './BlogArchivePage';
+export { default as RegisterUserTestPage } from './RegisterUserTestPage';
