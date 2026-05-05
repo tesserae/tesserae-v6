8ccd056cd29d69f16e973f99900ec16ec23184b5
Harika Krishnan Pentyala <HarikaKrishnanP@users.noreply.github.com>
2026-05-05 13:13:39 -0400

    Fix Corpus Scriptorum Latinorum redirecting to spam site

--- client/src/components/about/TextCredits.jsx
+++ client/src/components/about/TextCredits.jsx
@@ -6,7 +6,7 @@ const SOURCE_LINKS = {
   'DigilibLT': 'https://digiliblt.uniupo.it/',
   'Open Greek and Latin Project': 'https://opengreekandlatin.org/',
   'Musisque Deoque': 'http://www.mqdq.it/',
-  'Corpus Scriptorum Latinorum': 'http://www.forumromanum.org/literature/index.html',
+  'Corpus Scriptorum Latinorum': 'https://web.archive.org/web/20220305141011/http://www.forumromanum.org/literature/index.html',
 };
 
 function IntroLink({ name, url }) {
